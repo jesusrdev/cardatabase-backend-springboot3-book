b67342c78f0ed448d23b60b76c505518d902e7ad
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-08-26 18:19:03 -0500

    adding data with the CommandLineRunner

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -2,20 +2,26 @@ package com.packt.cardatabase;
 
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class CardatabaseApplication implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
-    private final CarRepository repository;
+    private final CarRepository carRepository;
+    private final OwnerRepository ownerRepository;
 
-    public CardatabaseApplication(CarRepository repository) {
-        this.repository = repository;
+    public CardatabaseApplication(CarRepository carRepository, OwnerRepository ownerRepository) {
+        this.carRepository = carRepository;
+        this.ownerRepository = ownerRepository;
     }
 
     public static void main(String[] args) {
@@ -26,12 +32,17 @@ public class CardatabaseApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000));
-        repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000));
-        repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000));
+        // Add owner objects and save these to db
+        Owner owner1 = new Owner("John", "Johnson");
+        Owner owner2 = new Owner("Mary", "Robinson");
+        ownerRepository.saveAll(Arrays.asList(owner1, owner2));
+
+        carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000, owner1));
+        carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000, owner2));
+        carRepository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000, owner2));
 
         // Fetch all cars and log to console
-        for (Car car : repository.findAll()) {
+        for (Car car : carRepository.findAll()) {
             logger.info("brand: {}, model: {}", car.getBrand(), car.getModel());
         }
     }
