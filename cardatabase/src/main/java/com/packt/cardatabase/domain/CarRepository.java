ff07d95571b48c1667db0d36a35f1c6a686eaac1
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-08-26 17:40:33 -0500

    adding defined queries to fetch data

--- cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -2,6 +2,38 @@ package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 //*                                                 <Entity, id type>
 public interface CarRepository extends CrudRepository<Car, Long> {
+
+    /*
+
+    * Example of defined queries in Spring Boot using findBy and orderBy and @Query
+
+    // Fetch cars by brand
+    List<Car> findByBrand(String brand);
+
+    // Fetch cars by color
+    List<Car> findByColor(String color);
+
+    // Fetch cars by model year
+    List<Car> findByModelYear(int modelYear);
+
+    // Fetch cars by brand and model
+    List<Car> findByBrandAndModel(String brand, String model);
+
+    // Fetch cars by brand or color
+    List<Car> findByBrandOrColor(String brand, String color);
+
+    // Fetch cars by brand and sort by year
+    List<Car> findByBrandOrderByModelYearAsc(String brand);
+
+    // Fetch cars by brand using SQL
+    @Query("select c from Car c where c.brand = ?1")
+    List<Car> findByBrand(String brand);
+
+
+     */
+
 }
