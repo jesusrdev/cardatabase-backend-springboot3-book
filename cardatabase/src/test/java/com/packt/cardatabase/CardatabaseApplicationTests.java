af425617aa71a8b1313cbf8f656b04c243bd9328
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-09-17 09:51:37 -0500

    adding an example test case to test that the injected controller instance is not null

--- cardatabase/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
+++ cardatabase/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,21 @@
 package com.packt.cardatabase;
 
+import com.packt.cardatabase.web.CarController;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CardatabaseApplicationTests {
+	@Autowired
+	private CarController carController;
 
 	@Test
+	@DisplayName("First example test case")
 	void contextLoads() {
+		Assertions.assertThat(carController).isNotNull();
 	}
 
 }
